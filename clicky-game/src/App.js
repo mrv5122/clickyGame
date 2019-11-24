4d6757d810ea293ade32ca83faf5ad8dd0b4d2c1
Manuel R <mannyvazqsan1@icloud.com>
2019-11-23 20:10:26 -0500

    fixed cards, title

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -2,35 +2,33 @@ import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import friends from "./friends.json";
+import selected from "./friends.json";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    selected
   };
 
-  removeFriend = id => {
+  selectedCharacter = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
+    const selected = this.state.selected.filter(friend => friend.id !== id);
     // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+    this.setState({ selected });
   };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
+        <Title>Clicky Game</Title>
+        {this.state.selected.map(friend => (
           <FriendCard
-            removeFriend={this.removeFriend}
+            selectedCharacter={this.selectedCharacter}
             id={friend.id}
             key={friend.id}
             name={friend.name}
             image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
           />
         ))}
       </Wrapper>
--- clicky-game/src/components/FriendCard/index.js
+++ clicky-game/src/components/FriendCard/index.js
@@ -3,26 +3,19 @@ import "./style.css";
 
 function FriendCard(props) {
   return (
-    <div className="card">
+    <div className="card remove" onClick={() => props.selectedCharacter(props.id)}>
       <div className="img-container">
         <img alt={props.name} src={props.image} />
       </div>
+
       <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
-      </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
+
+            <strong>{props.name}</strong> 
+            <br/>
+            <span >
       </span>
+      </div>
+      
     </div>
   );
 }
